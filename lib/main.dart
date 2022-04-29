555f69e56432d520ea5502127fb16af6b0f655a8
piruty <8793641+piruty@users.noreply.github.com>
2022-04-29 21:00:45 +0900

    写真を取る & 画像を表示

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,6 @@
 // [【Flutter】カメラ機能を実装する](https://zenn.dev/mamushi/articles/flutter_camera)
+import 'dart:io';
+
 import 'package:flutter/material.dart';
 import 'package:camera/camera.dart';
 
@@ -76,16 +78,50 @@ class TakePictureScreenState extends State<TakePictureScreen> {
 
   @override
   Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: FutureBuilder<void>(
+          future: _initializeControllerFuture,
+          builder: (context, snapshot) {
+            if (snapshot.connectionState == ConnectionState.done) {
+              return CameraPreview(_controller);
+            } else {
+              return const Center(child: CircularProgressIndicator());
+            }
+          }
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () async {
+          // 写真を取る
+          final image = await _controller.takePicture();
+          // 表示画面に遷移
+          await Navigator.of(context).push(
+            MaterialPageRoute(
+              builder: (context) => DisplayPictureScreen(imagePath: image.path),
+              fullscreenDialog: true,
+            )
+          );
+        },
+        child: const Icon(Icons.camera_alt),
+      ),
+    );
     // FutureBuilderで初期化を待ってからプレビューを表示
-    return FutureBuilder<void>(
-      future: _initializeControllerFuture,
-      builder: (context, snapshot) {
-        if (snapshot.connectionState == ConnectionState.done) {
-          return CameraPreview(_controller);
-        } else {
-          return const Center(child: CircularProgressIndicator());
-        }
-      }
+  }
+}
+
+class DisplayPictureScreen extends StatelessWidget {
+  const DisplayPictureScreen({Key? key, required this.imagePath})
+    : super(key: key);
+
+  final String imagePath;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: const Text('取れた写真')),
+      body: Center(child: Image.file(File(imagePath)),)
     );
   }
 }
+
